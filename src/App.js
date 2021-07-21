26122700141770620553b06cdc81b5782f5a721d
Brian Paintsil <brnpaintsil@gmail.com>
2021-07-21 13:32:44 +0000

    version one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
+/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { productMapping } from './products';
+import * as XLSX from 'xlsx';
 
 function App() {
+  let composition = {};
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        // setData();
+        processExcel(reader.result);
+      };
+
+      reader.readAsBinaryString(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const processExcel = (d) => {
+    const wb = XLSX.read(d, { type: 'binary' });
+    const wsname = wb.SheetNames[0];
+    const ws = wb.Sheets[wsname];
+    /* Convert array of arrays */
+    const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+    data.map((d, index) => {
+      if (index > 0) {
+        trackPrdtComposition(d[0], d[1]);
+      }
+      return [];
+    });
+    writeToExcel();
+  };
+
+  const trackPrdtComposition = (prdtName) => {
+    const formattedPrdtName = prdtName.replace(/\s/g, '_').toLowerCase();
+    productMapping[formattedPrdtName].map((compItem) => {
+      if (composition.hasOwnProperty(compItem.name)) {
+        composition[compItem.name].push({
+          product: formattedPrdtName,
+          quantity: compItem.qty,
+        });
+      } else {
+        composition[compItem.name] = [
+          {
+            product: formattedPrdtName,
+            quantity: compItem.qty,
+          },
+        ];
+      }
+      return [];
+    });
+  };
+
+  const writeToExcel = () => {
+    const builder = jsonToSheetBuilder();
+    const ws = XLSX.utils.json_to_sheet(builder, {
+      header: ['A', 'B', 'C'],
+      skipHeader: true,
+    });
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Doughman Comp Breakdown');
+    XLSX.writeFile(wb, 'test.xlsx');
+  };
+
+  const jsonToSheetBuilder = () => {
+    const compKeys = Object.keys(composition);
+    let sheetArr = [{ A: 'Comp Item', B: 'Quantity Used', C: 'Break Down' }];
+    compKeys.forEach((cKey) => {
+      const sheetObj = {
+        A: cKey,
+        B: composition[cKey]
+          .map((comp) => comp.quantity)
+          .reduce((acc, val) => acc + val),
+        C: composition[cKey]
+          .map((comp) => `${comp.product}(${comp.quantity}) `)
+          .join(' '),
+      };
+      sheetArr.push(sheetObj);
+    });
+    return sheetArr;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,32 @@
+export const productMapping = {
+  jade_mini: [
+    { name: 'burger', qty: 1 },
+    { name: 'muffins', qty: 3 },
+    { name: 'samosas', qty: 2 },
+    { name: 'spring rolls', qty: 2 },
+    { name: 'doughnuts', qty: 2 },
+  ],
+  jade_maxi: [
+    { name: 'burger', qty: 2 },
+    { name: 'muffins', qty: 6 },
+    { name: 'samosas', qty: 4 },
+    { name: 'spring rolls', qty: 4 },
+    { name: 'doughnuts', qty: 4 },
+  ],
+  jade_solo: [
+    { name: 'muffins', qty: 1 },
+    { name: 'samosas', qty: 1 },
+    { name: 'spring rolls', qty: 1 },
+    { name: 'doughnuts', qty: 1 },
+  ],
+  ivory_mini: [
+    { name: 'muffins', qty: 3 },
+    { name: 'burger', qty: 2 },
+    { name: 'doughnuts', qty: 2 },
+  ],
+  ivory_maxi: [
+    { name: 'muffins', qty: 6 },
+    { name: 'burger', qty: 4 },
+    { name: 'doughnuts', qty: 4 },
+  ],
+};
